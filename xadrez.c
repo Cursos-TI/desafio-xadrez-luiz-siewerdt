c85951c6b8234d228785c1bf17e25c05e8329a38
Luiz Gustavo A. Siewerdt <105997677+luiz-siewerdt@users.noreply.github.com>
2025-03-26 03:57:03 +0000

    Nivel Aventureiro/Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,76 @@
 #include <stdio.h>
 
+// Definindo o tipo de função que será usada como callback para moverPeca
+// Deve ser um ponteiro para um ponteiro char, para que eu possa alterar o
+// ponteiro, com apenas um ponteiro, irei apenas conseguir
+// acessar.
+typedef void (*mudarDirecao)(char **direcao, int localAtual);
+
+void cavaloDirecao(char **direcao, int localAtual) {
+  if (localAtual >= 2) {
+    // Aqui estou alterando para aonde a direcao aponta.
+    *direcao = "Direita";
+  }
+};
+
+// O programador pode tanto passar uma função do tipo mudarDirecao, quanto um
+// ponteiro NULL caso não queira alterar a direção em algum momento.
+// Passando localAtual como ponteiro para poder alterar o valor pela função
+void moverPeca(char *direcao, int *localAtual, int *localFinal,
+               mudarDirecao callback) {
+  printf("%s; ", direcao);
+
+  if (*localAtual == *localFinal)
+    return;
+
+  (*localAtual)++;
+  if (callback != NULL) {
+    callback(&direcao, *localAtual);
+  }
+
+  moverPeca(direcao, localAtual, localFinal, callback);
+}
+
 int main() {
   // Movimentando com laço For
   printf("Movimentado Torre: \n");
-  for (size_t i = 0; i < 5; ++i) {
-    printf("Direita; ");
-  }
+  int torreLocalAtual = 0;
+  int torreLocalFinal = 5;
+  moverPeca("Direita", &torreLocalAtual, &torreLocalFinal, NULL);
 
   printf("\n\n");
 
   // Movimentando com While
   printf("Movimentado Bispo: \n");
-  int localBispo = 0;
+  int bispoLocalInicial = 0;
+  int bispoLocalFinal = 5;
+  moverPeca("Direita, Cima", &bispoLocalInicial, &bispoLocalFinal, NULL);
 
-  while (localBispo < 5) {
-    printf("Cima, Direita; ");
-    ++localBispo;
+  printf("\n\n");
+
+  printf("Movimentado Bispo(Loop Aninhado): \n");
+  int bispoAninhadoLocalFinal = 5;
+  for (int i = 0; i <= bispoLocalFinal; ++i) {
+    printf("Direita, ");
+    for (int j = i; j < i + 1; j++) {
+      printf("Cima; ");
+    }
   }
 
   printf("\n\n");
 
   // Movimentando com Do While
   printf("Movimentando a Rainha: \n");
-  int localRainha = 0;
+  int rainhaLocalAtual = 0;
+  int rainhaLocalFinal = 5;
+  moverPeca("Esquerda", &rainhaLocalAtual, &rainhaLocalFinal, NULL);
 
-  do {
-    printf("Esquerda; ");
-    ++localRainha;
-  } while (localRainha < 5);
+  printf("\n\n");
+
+  printf("Movimentando Cavalo: \n");
+  int cavaloLocalAtual = 0;
+  int cavaloLocalFinal = 2;
+  moverPeca("Cima", &cavaloLocalAtual, &cavaloLocalFinal, cavaloDirecao);
 
   printf("\n");
 
